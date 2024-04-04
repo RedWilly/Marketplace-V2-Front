44fb31d667977ad19c361aefe8ced18e37cba1fe
RedWilly <thompsonau023@gmail.com>
2024-04-04 09:46:07 -0400

    fixed display

--- src/utils/Nft.js
+++ src/utils/Nft.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const middlewareBaseURL = 'http://localhost:9001'; // Replace with your middleware's base URL this is a running on render free for testing
+const middlewareBaseURL = 'https://rooni.site'; // Replace with your middleware's base URL this is a running on render free for testing
 
 class Nft {
     constructor(chain, collection, id) {
